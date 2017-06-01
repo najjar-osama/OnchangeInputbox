ef675b6b132bbe83adb2e6139d04074075803a9e
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2017-06-01 15:05:58 +0200

    Cleanup code

--- src/OnChangeInputbox/widget/OnChangeInputbox.js
+++ src/OnChangeInputbox/widget/OnChangeInputbox.js
@@ -2,23 +2,23 @@ define([
     "dojo/_base/declare", "mxui/widget/_WidgetBase", "dijit/_TemplatedMixin",
     "mxui/dom", "dojo/dom", "dojo/query", "dojo/dom-prop", "dojo/dom-geometry", "dojo/dom-class", "dojo/dom-style", "dojo/dom-construct", "dojo/_base/array", "dojo/_base/lang",
     "dojo/text!OnChangeInputbox/widget/template/OnChangeInputbox.html"
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, dojoLang, widgetTemplate) {
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, dojoLang, widgetTemplate) {
     "use strict";
 
     return declare("OnChangeInputbox.widget.OnChangeInputbox", [_WidgetBase, _TemplatedMixin], {
-        templateString         : widgetTemplate,
+        templateString: widgetTemplate,
 
         //CACHES
-        _hasStarted     : false,
-        subHandle       : null,
-        divNode         : "",
-        inputBox        : "",
-        handle          : "",
-        delay_timer     : "",
-        currValue       : "",
-        obj             : null,
+        _hasStarted: false,
+        subHandle: null,
+        divNode: "",
+        inputBox: "",
+        handle: "",
+        delay_timer: "",
+        currValue: "",
+        obj: null,
 
-        startup : function(){
+        startup: function() {
             if (this._hasStarted) {
                 return;
             }
@@ -34,55 +34,61 @@ define([
 
             this.actLoaded();
         },
-        update : function(obj, callback){
+
+        update: function(obj, callback) {
             this.obj = obj;
             if (this.subHandle) {
                 this.unsubscribe(this.subHandle);
+                this.subHandle = null;
             }
-            if (this.obj) {
-              // in some cases update may be called with a null obj
-              this.subHandle = this.subscribe({
-                  guid : obj.getGuid(),
-                  attr : this.name,
-                  callback : dojoLang.hitch(this, function (obj) {
-                      this.changeInputBox();
-                  })
-              });
 
-              this.changeInputBox();
+            if (this.obj) {
+                // in some cases update may be called with a null obj
+                this.subHandle = this.subscribe({
+                    guid: obj.getGuid(),
+                    attr: this.name,
+                    callback: dojoLang.hitch(this, function(obj) {
+                        this.changeInputBox();
+                    })
+                });
+                this.changeInputBox();
             }
 
             if (callback) {
                 callback();
             }
         },
-        changeInputBox : function () {
+
+        changeInputBox: function() {
             this.inputBox.value = this.obj.get(this.name);
         },
-        eventInputFocus : function () {
+
+        eventInputFocus: function() {
             domClass.add(this.inputBox, "MxClient_formFocus");
         },
-        eventOnChange : function() {
-			if (this.obj.get(this.name) !== this.inputBox.value) {
-				this.obj.set(this.name, this.inputBox.value);
-				mx.data.save({
-					mxobj : this.obj,
-					callback : dojoLang.hitch(this, function () {
-						// CHECK TRESHOLD HERE.
-						if (this.chartreshold > 0) {
-							if (this.inputBox.value.length > this.chartreshold){
-								this.eventCheckDelay();
-							} else {
-								clearTimeout(this.delay_timer);
-							}
-						} else {
-							this.eventCheckDelay();
-						}
-					})
-				});
-			}
+
+        eventOnChange: function() {
+            if (this.obj.get(this.name) !== this.inputBox.value) {
+                this.obj.set(this.name, this.inputBox.value);
+                mx.data.save({
+                    mxobj: this.obj,
+                    callback: dojoLang.hitch(this, function() {
+                        // CHECK TRESHOLD HERE.
+                        if (this.chartreshold > 0) {
+                            if (this.inputBox.value.length > this.chartreshold) {
+                                this.eventCheckDelay();
+                            } else {
+                                clearTimeout(this.delay_timer);
+                            }
+                        } else {
+                            this.eventCheckDelay();
+                        }
+                    })
+                });
+            }
         },
-        eventCheckDelay : function () {
+
+        eventCheckDelay: function() {
             if (this.delay > 0) {
                 if (this.delay_timer) {
                     clearTimeout(this.delay_timer);
@@ -91,31 +97,31 @@ define([
             } else {
                 this.onChangeMicroflow();
             }
-
         },
-        onChangeMicroflow : function () {
+
+        onChangeMicroflow: function() {
             this.delay_timer = null;
             this.executeMicroflow(this.onchangemf);
         },
-        onLeaveMicroflow : function () {
+
+        onLeaveMicroflow: function() {
             this.delay_timer = null;
             this.executeMicroflow(this.onleavemf);
         },
-        executeMicroflow : function (mf) {
+
+        executeMicroflow: function(mf) {
             if (mf && this.obj) {
                 mx.data.action({
                     store: {
-                       caller: this.mxform
+                        caller: this.mxform
                     },
                     params: {
-                        actionname  : mf,
-                        applyto     : "selection",
-                        guids       : [this.obj.getGuid()]
+                        actionname: mf,
+                        applyto: "selection",
+                        guids: [this.obj.getGuid()]
                     },
-                    callback: function () {
-                        // ok
-                    },
-                    error: function () {
+                    callback: function() {},
+                    error: function() {
                         logger.error("OnChangeInputbox.widget.OnChangeInputbox.triggerMicroFlow: XAS error executing microflow");
                     }
                 });
--- src/OnChangeInputbox/widget/OnChangeTextarea.js
+++ src/OnChangeInputbox/widget/OnChangeTextarea.js
@@ -1,22 +1,22 @@
 define([
     "dojo/_base/declare", "mxui/widget/_WidgetBase",
     "mxui/dom", "dojo/dom", "dojo/query", "dojo/dom-prop", "dojo/dom-geometry", "dojo/dom-class", "dojo/dom-style", "dojo/dom-construct", "dojo/_base/array", "dojo/_base/lang"
-], function (declare, _WidgetBase, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, dojoLang) {
+], function(declare, _WidgetBase, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, dojoLang) {
     "use strict";
 
     return declare("OnChangeInputbox.widget.OnChangeTextarea", [_WidgetBase], {
         //CACHES
-        _hasStarted  : false,
-        divNode      : "",
-        inputBox     : "",
-        handle       : "",
-        delay_timer  : "",
-        currValue    : "",
-        objId        : null,
-        textarea     : null,
-        _textLocked  : false,
+        _hasStarted: false,
+        divNode: "",
+        inputBox: "",
+        handle: "",
+        delay_timer: "",
+        currValue: "",
+        objId: null,
+        textarea: null,
+        _textLocked: false,
 
-        startup : function () {
+        startup: function() {
             if (this._hasStarted) {
                 return;
             }
@@ -26,17 +26,17 @@ define([
             var taNode = mxui.dom.div();
             this.domNode.appendChild(taNode);
             this.textarea = new mxui.widget.TextArea({
-                attributePath : this.entity + "/" + this.attr,
-                placeholder : this.placeholder,
-                maxLength : this.maxLength,
-                rows : 0,
-                textTooLongMsg : "",
-                counterMsg : ""
+                attributePath: this.entity + "/" + this.attr,
+                placeholder: this.placeholder,
+                maxLength: this.maxLength,
+                rows: 0,
+                textTooLongMsg: "",
+                counterMsg: ""
             }, taNode);
             this.textarea.startup();
 
             var self = this;
-            this.textarea._setValueAttr = function () {
+            this.textarea._setValueAttr = function() {
                 if (!self._textLocked) {
                     this.editNode.value = arguments[0];
                 }
@@ -49,35 +49,38 @@ define([
             this.actLoaded();
         },
 
-        update : function(obj, callback) {
+        update: function(obj, callback) {
             this.obj = obj;
             this.textarea.update(obj, callback);
         },
-        eventInputFocus : function () {
+
+        eventInputFocus: function() {
             domClass.add(this.inputBox, "MxClient_formFocus");
         },
-        eventOnChange : function() {
-			if (this.obj.get(this.name) !== this.inputBox.value) {
-				this._textLocked = true;
-				this.obj.set(this.attr, this.textarea.editNode.value);
-				mx.data.save({
-					mxobj : this.obj,
-					callback : dojoLang.hitch(this, function () {
-						// CHECK TRESHOLD HERE.
-						if (this.chartreshold > 0) {
-							if (this.textarea.editNode.value.length > this.chartreshold) {
-								this.eventCheckDelay();
-							} else {
-								clearTimeout(this.delay_timer);
-							}
-						} else {
-							this.eventCheckDelay();
-						}
-					})
-				});
-			}
+
+        eventOnChange: function() {
+            if (this.obj.get(this.name) !== this.inputBox.value) {
+                this._textLocked = true;
+                this.obj.set(this.attr, this.textarea.editNode.value);
+                mx.data.save({
+                    mxobj: this.obj,
+                    callback: dojoLang.hitch(this, function() {
+                        // CHECK TRESHOLD HERE.
+                        if (this.chartreshold > 0) {
+                            if (this.textarea.editNode.value.length > this.chartreshold) {
+                                this.eventCheckDelay();
+                            } else {
+                                clearTimeout(this.delay_timer);
+                            }
+                        } else {
+                            this.eventCheckDelay();
+                        }
+                    })
+                });
+            }
         },
-        eventCheckDelay : function () {
+
+        eventCheckDelay: function() {
             if (this.delay > 0) {
                 if (this.delay_timer) {
                     clearTimeout(this.delay_timer);
@@ -88,27 +91,28 @@ define([
             } else {
                 this.onChangeMicroflow();
             }
-
         },
-        onChangeMicroflow : function () {
+
+        onChangeMicroflow: function() {
             this.delay_timer = null;
             this.executeMicroflow(this.onchangemf);
         },
-        executeMicroflow : function (mf) {
+
+        executeMicroflow: function(mf) {
             if (mf && this.obj) {
                 mx.data.action({
                     store: {
-                       caller: this.mxform
+                        caller: this.mxform
                     },
                     params: {
-                        actionname  : mf,
-                        applyto     : "selection",
-                        guids       : [this.obj.getGuid()]
+                        actionname: mf,
+                        applyto: "selection",
+                        guids: [this.obj.getGuid()]
                     },
-                    callback : dojoLang.hitch(this, function () {
+                    callback: dojoLang.hitch(this, function() {
                         this._textLocked = false;
                     }),
-                    error: function () {
+                    error: function() {
                         logger.error("OnChangeInputbox.widget.OnChangeTextarea.triggerMicroFlow: XAS error executing microflow");
                     }
                 });
